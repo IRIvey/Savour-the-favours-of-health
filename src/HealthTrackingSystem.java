e06be7951c37125081505a9efdebda5e1605f90e
IRIvey <isrativey@iut-dhaka.edu>
2025-03-26 09:27:38 +0600

    File Modified

--- src/HealthTrackingSystem.java
+++ src/HealthTrackingSystem.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
 
+
 public class HealthTrackingSystem {
     private static HealthTrackingSystem instance;
     private final CommandExecutor commandExecutor;
     private final MenuDisplay menuDisplay;
     private final Scanner scanner;
     private final User currentUser;
+    private final UserInputHandler userInputHandler;
 
-    private HealthTrackingSystem() {
+    private HealthTrackingSystem(UserInputHandler userInputHandler, CommandExecutor commandExecutor, MenuDisplay menuDisplay) {
         this.currentUser = new User("DefaultUser");
-        this.commandExecutor = new CommandExecutor();
-        this.menuDisplay = new MenuDisplay();
+        this.commandExecutor = commandExecutor;
+        this.menuDisplay = menuDisplay;
         this.scanner = new Scanner(System.in);
+        this.userInputHandler = userInputHandler;
     }
 
     public static HealthTrackingSystem getInstance() {
         if (instance == null) {
-            instance = new HealthTrackingSystem();
+            instance = new HealthTrackingSystem(new UserInputHandler(), new CommandExecutor(), new MenuDisplay());
         }
         return instance;
     }
@@ -25,14 +28,11 @@ public class HealthTrackingSystem {
     public void start() {
         while (true) {
             menuDisplay.showMenu();
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
+            int choice = userInputHandler.getUserChoice();
             if (choice == 0) {
-                System.out.println("Thank you for using the Health Tracking System!");
+                System.out.println("Thank you for using Health Tracking System!");
                 break;
             }
-
             commandExecutor.executeCommand(choice, currentUser);
         }
     }
