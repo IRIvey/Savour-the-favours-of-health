9e3fad9a29f23b8e0f36366105d47f3a579b4c75
IRIvey <isrativey@iut-dhaka.edu>
2025-03-26 09:30:31 +0600

    File Modified

--- src/CommandExecutor.java
+++ src/CommandExecutor.java
@@ -1,25 +1,28 @@
-public class CommandExecutor {
-    private final TrackerFactory trackerFactory = TrackerFactory.getInstance();
+import java.util.HashMap;
+import java.util.Map;
 
-    public void executeCommand(int choice, User user) {
-        HealthMetricType type = getHealthMetricType(choice);
-        if (type != null) {
-            Tracker tracker = trackerFactory.createTracker(type);
-            tracker.track(user);
-        } else {
-            System.out.println("Invalid choice! Please try again.");
-        }
+
+class CommandExecutor {
+    private final TrackerFactory trackerFactory = TrackerFactory.getInstance();
+    private final Map<Integer, HealthMetricType> commandMap;
+
+    public CommandExecutor() {
+        commandMap = new HashMap<>();
+        commandMap.put(1, HealthMetricType.WATER_INTAKE);
+        commandMap.put(2, HealthMetricType.SLEEP_DURATION);
+        commandMap.put(3, HealthMetricType.EXERCISE_DURATION);
+        commandMap.put(4, HealthMetricType.CALORIES_CONSUMED);
+        commandMap.put(5, HealthMetricType.WEIGHT);
+        commandMap.put(6, HealthMetricType.STEPS_TAKEN);
     }
 
-    private HealthMetricType getHealthMetricType(int choice) {
-        return switch (choice) {
-            case 1 -> HealthMetricType.WATER_INTAKE;
-            case 2 -> HealthMetricType.SLEEP_DURATION;
-            case 3 -> HealthMetricType.EXERCISE_DURATION;
-            case 4 -> HealthMetricType.CALORIES_CONSUMED;
-            case 5 -> HealthMetricType.WEIGHT;
-            case 6 -> HealthMetricType.STEPS_TAKEN;
-            default -> null;
-        };
+    public void executeCommand(int choice, User user) {
+        HealthMetricType metricType = commandMap.get(choice);
+        if (metricType != null) {
+            Tracker tracker = trackerFactory.createTracker(metricType);
+            tracker.track(user);
+        } else {
+            System.out.println("Invalid command choice: " + choice);
+        }
     }
 }
