f860ea50384768b55487ac8def105eda04a7b13a
IRIvey <isrativey@iut-dhaka.edu>
2025-03-27 21:59:41 +0600

    File Modified

--- src/GoalPeriod.java
+++ src/GoalPeriod.java
@@ -1,15 +1,3 @@
-public enum GoalPeriod {
-    DAILY("Daily"),
-    WEEKLY("Weekly"),
-    MONTHLY("Monthly");
-
-    private final String displayName;
-
-    GoalPeriod(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
+interface GoalPeriod {
+    String getPeriodName();
 }
